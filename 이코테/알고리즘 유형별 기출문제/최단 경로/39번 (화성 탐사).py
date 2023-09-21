55624e1cc0b27cb3b501aeba12fffb1fd37d1e5b
맹순영 <symaeng98@naver.com>
2023-09-21 11:04:03 +0900

    [이코테] 알고리즘 유형별 기출 문제 - 39번(화성 탐사)

---
+++ 이코테/알고리즘 유형별 기출문제/최단 경로/39번 (화성 탐사).py
@@ -0,0 +1,31 @@
+import heapq
+
+INF = 1e9
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    graph = [list(map(int,input().split())) for _ in range(n)]
+
+    distance = [[INF]*n for _ in range(n)]
+
+    x, y = 0, 0
+    q = [(graph[x][y], x, y)]
+    distance[x][y] = graph[x][y]
+
+    while q:
+        dist, x, y = heapq.heappop(q)
+        if distance[x][y] < dist:
+            continue
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < n:
+                cost = dist + graph[nx][ny]
+                if cost < distance[nx][ny]:
+                    distance[nx][ny] = cost
+                    heapq.heappush(q, (cost, nx, ny))
+
+    print(distance[n-1][n-1])
