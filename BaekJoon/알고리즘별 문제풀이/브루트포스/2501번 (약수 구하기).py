c1fb27cb1f048912b207a882777d68a339ee2839
맹순영 <symaeng98@naver.com>
2024-01-12 13:40:10 +0900

    백준 [브루트포스] 2501번 (약수 구하기)

---
+++ BaekJoon/알고리즘별 문제풀이/브루트포스/2501번 (약수 구하기).py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+result = []
+for i in range(1, n+1):
+    if n % i == 0:
+        result.append(i)
+
+if len(result) < k:
+    print(0)
+else:
+    print(result[k-1])
