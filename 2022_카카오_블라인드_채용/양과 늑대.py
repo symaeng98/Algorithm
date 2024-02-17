be628f6e4101ef2375a43d4f4324f58247745b0b
맹순영 <symaeng98@naver.com>
2024-02-17 10:26:00 +0900

    2022 카카오 블라인드 채용 - 양과 늑대

---
+++ 2022_카카오_블라인드_채용/양과 늑대.py
@@ -0,0 +1,26 @@
+def solution(info, edges):
+    visited = [False] * len(info)
+    answer = []
+
+    def dfs(sheep, wolf):
+        if sheep > wolf:
+            answer.append(sheep)
+        else:
+            return
+
+        for parent, child in edges:
+            if visited[parent] and not visited[child]:
+                visited[child] = True
+                if info[child] == 0:
+                    dfs(sheep+1, wolf)
+                else:
+                    dfs(sheep, wolf+1)
+                visited[child] = False
+
+    visited[0] = True
+    dfs(1, 0)
+
+    return max(answer)
+
+print(solution([0,0,1,1,1,0,1,0,1,0,1,1], [[0,1],[1,2],[1,4],[0,8],[8,7],[9,10],[9,11],[4,3],[6,5],[4,6],[8,9]]))
+print(solution([0,1,0,1,1,0,1,0,0,1,0], [[0,1],[0,2],[1,3],[1,4],[2,5],[2,6],[3,7],[4,8],[6,9],[9,10]]))
