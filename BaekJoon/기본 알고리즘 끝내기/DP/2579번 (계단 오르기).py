70520ab2e2d06484b3c2a8279d84093dacf0477e
맹순영 <symaeng98@naver.com>
2024-05-05 22:40:50 +0900

    [백준] 기본 알고리즘 끝내기 - 2579번 (계단 오르기)

---
+++ BaekJoon/기본 알고리즘 끝내기/DP/2579번 (계단 오르기).py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = [0]*10001
+dp = [0]*10001
+for i in range(n):
+    arr[i] = int(input())
+
+dp[0] = arr[0]
+dp[1] = arr[0]+arr[1]
+dp[2] = max(arr[0]+arr[2], arr[1]+arr[2])
+for i in range(3, n):
+    dp[i] = max(dp[i-3]+arr[i-1], dp[i-2]) + arr[i]
+
+print(dp[n-1])
