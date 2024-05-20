2e7bd077909e7be205901b770bcf5d159f080078
맹순영 <symaeng98@naver.com>
2024-05-20 16:12:13 +0900

    [프로그래머스] 2021 카카오 블라인드 채용 - 카드 짝 맞추기 (구현, 그래프)

--- 프로그래머스/2021_카카오_블라인드_채용/카드 짝 맞추기.py
+++ 프로그래머스/2021_카카오_블라인드_채용/카드 짝 맞추기.py
@@ -16,7 +16,7 @@ def solution(board, r, c):
                     pair[k].append((i, j))
     print(total_card_num)
 
-    def xy_to_txy(x, y, tx, ty):
+    def xy_to_txy(board, x, y, tx, ty):
         distance = [[0]*4 for _ in range(4)]
         distance[x][y] = 0
         for i in range(4):
@@ -28,16 +28,16 @@ def solution(board, r, c):
         visited = [(x, y)]
         while q:
             cost, x, y = q.popleft()
-            # for i in range(4):
-            #     nx = x + dx[i]
-            #     ny = y + dy[i]
-            #     if 0 <= nx < 4 and 0 <= ny < 4:
-
-
-
             for i in range(4):
                 nx = x + dx[i]
                 ny = y + dy[i]
+                mx = x + dx[i]
+                my = y + dy[i]
+                if 0 <= mx < 4 and 0 <= my < 4:
+                    if (mx, my) not in visited:
+                        visited.append((mx, my))
+                        distance[mx][my] = cost + 1
+                        q.append((cost+1, mx, my))
                 while True:
                     if 0 <= nx < 4 and 0 <= ny < 4:
                         if board[nx][ny] != 0:
@@ -47,42 +47,46 @@ def solution(board, r, c):
                     nx -= dx[i]
                     ny -= dy[i]
                     break
-                if nx == x and ny == y:
-                    continue
-                distance[nx][ny] = min(distance[nx][ny], distance[x][y]+1)
-                q.append((distance[nx][ny], nx, ny))
-        print(distance)
+                # if nx == x and ny == y:
+                #     continue
+                if (nx, ny) not in visited:
+                    visited.append((nx, ny))
+                    distance[nx][ny] = cost + 1
+                    q.append((cost+1, nx, ny))
+        # print(distance)
         return distance[tx][ty]
 
-
+    print(pair)
     for per in permutations([i for i in range(1, total_card_num+1)]):
         result = []
         tmp = copy.deepcopy(board)
         cost = 0
         for now in per:
-            p1 = xy_to_txy(r, c, pair[now][0][0], pair[now][0][1])
-            p2 = xy_to_txy(r, c, pair[now][1][0], pair[now][1][1])
+            p1 = xy_to_txy(tmp, r, c, pair[now][0][0], pair[now][0][1])
+            p2 = xy_to_txy(tmp, r, c, pair[now][1][0], pair[now][1][1])
+            print(p1, p2)
             if p1 < p2:
                 cost += p1
                 r, c = pair[now][0][0], pair[now][0][1]
 
                 cost += 1
-                cost += xy_to_txy(r, c, pair[now][1][0], pair[now][1][1])
+                cost += xy_to_txy(tmp, r, c, pair[now][1][0], pair[now][1][1])
                 r, c = pair[now][1][0], pair[now][1][1]
                 cost += 1
-                board[pair[now][0][0]][pair[now][0][1]] = 0
-                board[pair[now][1][0]][pair[now][1][1]] = 0
+                tmp[pair[now][0][0]][pair[now][0][1]] = 0
+                tmp[pair[now][1][0]][pair[now][1][1]] = 0
             else:
                 cost += p2
                 r, c = pair[now][1][0], pair[now][1][1]
 
                 cost += 1
-                cost += xy_to_txy(r, c, pair[now][0][0], pair[now][0][1])
+                cost += xy_to_txy(tmp, r, c, pair[now][0][0], pair[now][0][1])
                 r, c = pair[now][0][0], pair[now][0][1]
                 cost += 1
-                board[pair[now][0][0]][pair[now][0][1]] = 0
-                board[pair[now][1][0]][pair[now][1][1]] = 0
+                tmp[pair[now][0][0]][pair[now][0][1]] = 0
+                tmp[pair[now][1][0]][pair[now][1][1]] = 0
 
-            result.append(cost)
+        result.append(cost)
 
+    print(result)
     return result
