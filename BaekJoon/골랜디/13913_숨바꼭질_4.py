3f180fb3e4e13a05cc0e26abbaae0857e54fb47d
맹순영 <symaeng98@naver.com>
2025-02-12 14:35:48 +0900

    [백준] 골랜디 - 13913_숨바꼭질_4.py

---
+++ BaekJoon/골랜디/13913_숨바꼭질_4.py
@@ -0,0 +1,30 @@
+import copy
+from collections import deque
+
+n, k = map(int, input().split())
+
+parent = [-1]*100001
+
+q = deque()
+q.append((0, n))
+parent[n] = n
+while q:
+    cnt, now = q.popleft()
+    if now == k:
+        print(cnt)
+        break
+
+    for x in [now+1, now*2, now-1]:
+        if 0 <= x <= 100000 and parent[x] == -1:
+            parent[x] = now
+            q.append((cnt+1, x))
+
+
+x = k
+result = [k]
+while x != n:
+    x = parent[x]
+    result.append(x)
+
+result.reverse()
+print(*result)
