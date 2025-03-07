315773effd9ba00abaaca96bff05849dc0ef79b2
맹순영 <symaeng98@naver.com>
2025-03-07 11:20:21 +0900

    [백준] 골랜디 - 7579_앱.py

--- BaekJoon/골랜디/7579_앱.py
+++ BaekJoon/골랜디/7579_앱.py
@@ -3,28 +3,27 @@ a = list(map(int, input().split()))
 c = list(map(int, input().split()))
 a.insert(0, 0)
 c.insert(0, 0)
+
+tmp = [x for x in range(sum(c)+1)]
 dp = [[0]*(sum(c)+1) for _ in range(n+1)]
-result = sum(c)
 
 for i in range(1, n+1):
-    for j in range(sum(c)+1):
-        if j < c[i]:
+    for j in range(len(tmp)):
+        k = c[i]
+        w = a[i]
+        if j < k:
             dp[i][j] = dp[i-1][j]
         else:
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-c[i]]+a[i])
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-k]+w)
 
+result = 1e9
+for i in range(n+1):
+    for j in range(len(tmp)):
         if dp[i][j] >= m:
             result = min(result, j)
 
-# print(dp)
-if m != 0:
-    print(result)
-else:
-    print(0)
+print(result)
+
 # 5 60
 # 30 10 20 35 40
 # 3 0 3 5 4
-
-# 5 60
-# 30 10 20 40 60
-# 3 0 3 4 5
