75d6ee3fbdfe8837010bfbb4b6c6c153f01cc8e2
맹순영 <symaeng98@naver.com>
2025-03-19 21:25:35 +0900

    [프로그래머스] 삼랜디 - 기지국_설치.py

--- 프로그래머스/삼랜디/기지국_설치.py
+++ 프로그래머스/삼랜디/기지국_설치.py
@@ -1,4 +1,4 @@
-# 9:00 ~ 9:21
+# 9:00
 def solution(n, stations, w):
     answer = 0
     blanks = []
@@ -6,15 +6,13 @@ def solution(n, stations, w):
     for station in stations:
         st_start = station-w
         st_end = station+w
-        if st_start <= bl_start <= st_end:
-            bl_start = st_end+1
-            continue
         blanks.append((bl_start, st_start-1))
         bl_start = st_end+1
-    if bl_start <= n:
-        blanks.append((bl_start, n))
-
+    blanks.append((bl_start, n))
+    print(blanks)
     for s, e in blanks:
+        if s > e:
+            continue
         answer += (e-s+1) // (2*w+1)
         if (e-s+1) % (2*w+1) == 0:
             continue
